374ae2a1b5744fa42c4f2c19a3ae39dbab46bf58
Willem Handreck <whreck1972@live.com>
2014-11-04 22:37:38 +1100

    Create better menu touch target

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $(document).on('touchstart click', '#collapse-button, #close-list', function(e){
         e.preventDefault();
-        $('#collapse-button span').toggleClass('hidden');
+        // $('#collapse-button span').toggleClass('hidden');
         $('#nav-collapse').collapse('toggle');
     });
 });
