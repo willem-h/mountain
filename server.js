d241f711588550b6fccaf35ee23e36a4993c9b54
Willem Handreck <whreck1972@live.com>
2014-11-28 21:01:25 +1100

    Add proper favicon support

--- server.js
+++ server.js
@@ -3,15 +3,35 @@ var compress = require('compression'),
     express = require('express'),
     app = express(),
     server = require('http').createServer(app),
+    crypto = require('credential'),
+    database = require('./database'),
     io = require('socket.io'),
     io = io(server);
 
 // Setup Express to compress data and be the server
 app.use(compress({ threshold: 512}));
 
+io.on('connection', function(socket){
+    console.log("A user connected");
+
+    socket.on('hello', function(data){  // Fake one so I remember
+        console.log(data);
+    });
+
+    socket.on('newUser', function(data){
+        
+    });
+
+    socket.on('disconnect', function(){
+        console.log('A user disconnected');
+    });
+});
+
+if (database) console.log("DB is up and running");
+
 app.use(express.static(__dirname + '/dist', 'binary'));
 
-var port = Number(process.env.PORT || 5000);
+var port = Number(process.env.PORT || 5001);
 server.listen(port, function(){
     console.log("Listening on "+ port);
 });
--- src/scripts/script.js
+++ src/scripts/script.js
@@ -4,4 +4,38 @@ $(document).ready(function(){
         // $('#collapse-button span').toggleClass('hidden');
         $('#nav-collapse').collapse('toggle');
     });
+
+    $(document).on('touchstart click', '#login-btn', function(e){
+        e.preventDefault();
+
+        $('#login-div, #callToAction').toggleClass('hidden');
+        $('#login-email').focus();
+    });
+
+    $(document).on('touchstart click', '#sign-up-btn', function(e){
+        e.preventDefault();
+
+        $('#sign-up-div, #callToAction').toggleClass('hidden');
+    });
+
+    $(document).on('submit', '#login-form', function(e){
+        e.preventDefault();
+
+        var email = $('#login-email').val();
+        var password = $('#login-password').val();
+
+        // if (e.currentTarget)
+    });
+
+    $(document).on('touchstart click', '#loginFormReset', function(e){
+        e.preventDefault();
+
+        $('#login-div, #callToAction').toggleClass('hidden');
+    });
+
+    $(document).on('touchstart click', '#signUpFormReset', function(e){
+        e.preventDefault();
+
+        $('#sign-up-div, #callToAction').toggleClass('hidden');
+    });
 });
